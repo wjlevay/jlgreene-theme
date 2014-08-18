f12441f636b2829bdc402a9facdd4b6cd6415dae
wjlevay <wjlevay@gmail.com>
2014-08-18 12:56:07 -0400

    Fix to "hide nags" function

--- functions.php
+++ functions.php
@@ -119,15 +119,13 @@ new image size.
 
 // via http://www.wpoptimus.com/626/7-ways-disable-update-wordpress-notifications/
 
-if(!current_user_can('update_core')) {
-  function remove_core_updates(){
-    global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
-  }
-
-  add_filter('pre_site_transient_update_core','remove_core_updates');
-  add_filter('pre_site_transient_update_plugins','remove_core_updates');
-  add_filter('pre_site_transient_update_themes','remove_core_updates');
+function remove_core_updates(){
+  global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
+
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
 
 
 /************* ACTIVE SIDEBARS ********************/
